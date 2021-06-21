316d1158b39340cae3b72dcd2104d193df3119f4
Manas Mohamed <manas.mohamed@wecreate.com.hk>
2021-06-21 16:32:42 +0800

    fixed button variable

--- wp-content/themes/wecreate/template-parts/main-details.php
+++ wp-content/themes/wecreate/template-parts/main-details.php
@@ -23,13 +23,16 @@
 
                 if ($button_text && $button_url) {
                     $button = '<a href="'.$button_url.'" class="button-links">'. __($button_text, 'eatology').'</a>';
-                }                
 
-                $content_div = '<div class="content-wrapper">
+                    $content_div = '<div class="content-wrapper">
                     <h2>'.get_the_title().'</h2>
                         <p>'.$content.'</p>
                         '.$button.'
                     </div>';
+
+                }                
+
+                
                 ?>
 
 
