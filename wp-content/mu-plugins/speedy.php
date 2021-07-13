1a2748301540560a5a5f58d619854cdd612cd953
James Songalia <jamezz24@yahoo.com>
2021-07-13 22:52:24 +0800

    FIX: jQuery

--- wp-content/mu-plugins/speedy.php
+++ wp-content/mu-plugins/speedy.php
@@ -81,6 +81,10 @@ add_action('wp_enqueue_scripts', function() {
         wp_dequeue_style( 'wp-block-library' );
         wp_dequeue_style( 'wp-block-library-theme' );
         wp_dequeue_style( 'wc-block-style' );
+        
+        // jQuery
+        wp_dequeue_script( 'jquery-migrate-js' );
+        wp_dequeue_script( 'jquery-core-js' );
 
         // CF7
         wp_dequeue_script( 'google-recaptcha' );
