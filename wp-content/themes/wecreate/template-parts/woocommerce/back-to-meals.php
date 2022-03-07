33426fad118662a99fddbb8655341c2008e7ebf8
Ayrah Otero <ayrah@administrators-iMac-2.local>
2022-03-07 19:26:00 +0800

    Fixed Labela and Product redirection

--- wp-content/themes/wecreate/template-parts/woocommerce/back-to-meals.php
+++ wp-content/themes/wecreate/template-parts/woocommerce/back-to-meals.php
@@ -1,20 +1,31 @@
 <?php 
+
+$url = $_SERVER['REQUEST_URI'];
+$url = explode("/",$url);
+
 if (ICL_LANGUAGE_CODE == 'zh')
 {
-	$shop_page_id = 3215;
+	$shop_page_id = "/zh/".$url[2];
 }
 else {
-	$shop_page_id = 451;
+	$shop_page_id = "/".$url[2];
+}
+
+if($url[2] == "wellness-boutique"){
+    $title_header = "BACK TO WELLNESS BOUTIQUE";
+}else{
+    $title_header = "OUR MEAL PLANS";
 }
 ?>
 
 <?php
+
 echo '<script>
     var extra_days_info = "'.__('5 Days are delivered only on weekdays.<br>6 Days are delivered every day except Sundays.', 'eatology').'";
     var per_days_text = "'.__(' PER DAY', 'eatology').'";
 </script>
 <div class="back-to-meal-plans">
-    <a href="'.get_the_permalink($shop_page_id). '">
-        <span class="icon-icon-back"></span> '.__('Our Meal Plans','eatology').'
+    <a href="'.$shop_page_id. '">
+        <span class="icon-icon-back"></span> '.__($title_header,'eatology').'
     </a>
 </div>';
